03c173b253a8a90d1b931baeff9267d258b80139
eeshaydv <eesha.yadav@flipkart.com>
2021-08-05 19:15:18 +0530

    sort logic added

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const slides = [
         alt="1"
       />
     ),
+    priority: 5,
   },
   {
     key: uuidv4(),
@@ -24,6 +25,7 @@ const slides = [
         alt="2"
       />
     ),
+    priority: 8,
   },
   {
     key: uuidv4(),
@@ -33,28 +35,67 @@ const slides = [
         alt="3"
       />
     ),
+    priority: 3,
   },
   {
     key: uuidv4(),
-    content: <img src="https://rukminim1.flixcart.com/flap/1200/280/image/3ef6b71c7dda2ebe.jpg?q=50" alt="4" />,
+    content: (
+      <img
+        src="https://rukminim1.flixcart.com/flap/1200/280/image/3ef6b71c7dda2ebe.jpg?q=50"
+        alt="4"
+      />
+    ),
+    priority: 4,
   },
   {
     key: uuidv4(),
-    content: <img src="https://rukminim1.flixcart.com/flap/1200/280/image/bbbf1d709468c0e4.jpg?q=50" alt="5" />,
+    content: (
+      <img
+        src="https://rukminim1.flixcart.com/flap/1200/280/image/bbbf1d709468c0e4.jpg?q=50"
+        alt="5"
+      />
+    ),
+    priority: 2,
   },
   {
     key: uuidv4(),
-    content: <img src="https://rukminim1.flixcart.com/flap/1200/280/image/a4aa90e138dde3c6.jpg?q=50" alt="6" />,
+    content: (
+      <img
+        src="https://rukminim1.flixcart.com/flap/1200/280/image/a4aa90e138dde3c6.jpg?q=50"
+        alt="6"
+      />
+    ),
+    priority: 6,
   },
   {
     key: uuidv4(),
-    content: <img src="https://rukminim1.flixcart.com/flap/1200/280/image/3ef6b71c7dda2ebe.jpg?q=50" alt="7" />,
+    content: (
+      <img
+        src="https://rukminim1.flixcart.com/flap/1200/280/image/3ef6b71c7dda2ebe.jpg?q=50"
+        alt="7"
+      />
+    ),
+    priority: 7,
   },
   {
     key: uuidv4(),
-    content: <img src="https://rukminim1.flixcart.com/flap/1200/280/image/3419f1a004b7504b.jpg?q=50" alt="8" />,
+    content: (
+      <img
+        src="https://rukminim1.flixcart.com/flap/1200/280/image/3419f1a004b7504b.jpg?q=50"
+        alt="8"
+      />
+    ),
+    priority: 1,
   },
 ];
+
+console.log(slides);
+
+slides.sort(function(a, b) {
+  return a.priority - b.priority;
+});
+
+console.log(slides);
 
 class App extends React.Component {
   constructor(props) {
@@ -73,7 +114,7 @@ class App extends React.Component {
       <div>
         <button onClick={this.handleClick}> Choose Carousel Orientation</button>
 
-        {this.state.showVertical ? <ExampleV /> : <Carousel slides={slides}  /> }
+        {this.state.showVertical ? <ExampleV /> : <Carousel slides={slides} />}
       </div>
     );
   }
